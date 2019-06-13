333a1a81e0d25e28a0fb189fc2e6b3a41b02ee10
Ole Hartvigsen <turforlag@users.noreply.github.com>
2019-06-13 20:44:27 +0200

    Update pdftk.rb

--- pdftk.rb
+++ pdftk.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class PkgExtract < CurlDownloadStrategy
   def stage
-    safe_system '/usr/bin/xar', '-xf', @tarball_path
+    safe_system '/usr/bin/xar', '-xf', $HOME/Library/Caches/Homebrew/downloads/
     chdir
     safe_system 'mv *.pkg/Payload Payload.gz'
     safe_system 'ls | grep -v Payload | xargs rm -r'
