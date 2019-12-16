c872ad5dbc1e54cf4e748e5528d04a021bdb5a26
Ole Hartvigsen <turforlag@users.noreply.github.com>
2019-12-16 12:18:28 +0000

    Update pdftk.rb

--- pdftk.rb
+++ pdftk.rb
@@ -15,10 +15,8 @@ class Pdftk < Formula
     :using => PkgExtract
   sha256 'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
 
-
-
   def install
-    safe_system "pax --insecure -rz -f Payload.gz -s ',./bin,#{bin},' -s ',./man,#{man},' -s ',./lib,#{lib},' -s ',./license_gpl_pdftk,#{prefix}/LICENSE,' -s ',./,#{prefix}/README/,'"
+    prefix.install Dir["pdftk/*"]
   end
 
   test do
