a5d9c0bf87b35e2f363e2423f72d92d5057b9398
Zander Hill <zander@xargs.io>
2021-05-14 20:07:11 -0700

    Fix "Error: Empty installation"
    Source: https://github.com/turforlag/homebrew-cervezas/pull/1#issuecomment-840964819
    
    Fixes installation by removing separate PkgExtract strategy. Then uses
    targeted bin.install and lib.install actions to avoid polluting other
    formulas with the dynamic library references.

--- pdftk.rb
+++ pdftk.rb
@@ -1,23 +1,22 @@
-class PkgExtract < CurlDownloadStrategy
-  def stage
-    url_sha256 = Digest::SHA256.hexdigest(url)
-    safe_system '/usr/bin/xar', '-xf', Dir["#{HOMEBREW_CACHE}/downloads/#{url_sha256}--*"].first
-    Dir.mkdir "pdftk"
-    safe_system "tar -xf *.pkg/Payload -C pdftk/."
-  end
-end
+# typed: false
+# frozen_string_literal: true
 
+# Pdftk formula
 class Pdftk < Formula
+  FILENAME = 'pdftk_server-2.02-mac_osx-10.11-setup.pkg'
   homepage 'http://www.pdflabs.com/tools/pdftk-server'
-  url 'http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg',
-    :using => PkgExtract
+  url "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/#{FILENAME}"
   sha256 'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
 
   def install
-    prefix.install Dir["pdftk/*"]
+    # Outputs it to pdftk.pkg/*
+    safe_system '/usr/bin/xar', '-xf', FILENAME
+    Dir.mkdir 'tmp'
+    safe_system 'tar -xf pdftk.pkg/Payload -C tmp/.'
+    prefix.install Dir['tmp/*']
   end
 
   test do
-    system "#{bin}/pdftk --version"
+    system "#{bin}/pdftk", '--version'
   end
 end
